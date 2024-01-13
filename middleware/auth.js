c808a2a6afb3e95433d388bd28c15dfc3776135a
Md Rasimul Islam <92817106+rasimul07@users.noreply.github.com>
2024-01-13 18:18:44 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,8 +5,9 @@ const authenticateJwt = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (authHeader) {
         const token = authHeader.split(' ')[1];
-        console.log('We are here inside the auth middleware.');
+        console.log('We are here inside the auth middleware.\nToken: '+token);
         jwt.verify(token, secret, (err, user) => {
+            console.log(user);
             if (err) {
                 return res.status(300).json({ message: "Authentication failed" });;
             }
